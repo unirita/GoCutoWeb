5a68bdadcb0f5205b24a0d015c23841b005f66dc
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-10-07 16:38:40 +0900

    Load config file in main routine.

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+
+	"github.com/unirita/gocutoweb/config"
 )
 
-func main() {
-	handler := setupHandler()
+type arguments struct {
+	configPath string
+}
 
-	if err := http.ListenAndServe(":8000", handler); err != nil {
-		fmt.Println(err)
+const defaultConfig = `web.ini`
+
+func main() {
+	args := fetchArgs()
+	if args.configPath == "" {
+		args.configPath = defaultConfig
 	}
+
+	if err := config.Load(args.configPath); err != nil {
+		fmt.Println("Could not load config:", err)
+	}
+
+	listenHost := fmt.Sprintf(":%d", config.Server.ListenPort)
+	if err := http.ListenAndServe(listenHost, setupHandler()); err != nil {
+		fmt.Println("Could not start to listen:", err)
+	}
+}
+
+func fetchArgs() *arguments {
+	args := new(arguments)
+	flag.StringVar(&args.configPath, "c", "", "Config file path")
+	flag.Parse()
+	return args
 }
