191385927290a973b4d26614339545d855f58dd8
katsuji_yamashita <katsuji_yamashita@unirita.co.jp>
2015-10-08 13:58:04 +0900

    Added API[noticeJobnet].

--- handler.go
+++ handler.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/gorilla/mux"
+	_ "github.com/unirita/gocutoweb/define"
 )
 
 const (
@@ -18,6 +19,7 @@ const (
 func setupHandler() http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/caches/{name:[0-9]{17}}", showJSONCache).Methods(methodGet)
+	router.HandleFunc("/notice", noticeJobnet).Methods(methodPost)
 	return router
 }
 
@@ -26,6 +28,7 @@ func showJSONCache(writer http.ResponseWriter, request *http.Request) {
 	name := params["name"]
 
 	cachePath := filepath.Join(os.TempDir(), "gocuto", name+".json")
+
 	file, err := os.Open(cachePath)
 	if err != nil {
 		writer.WriteHeader(http.StatusNotFound)
@@ -47,4 +50,22 @@ func showJSONCache(writer http.ResponseWriter, request *http.Request) {
 
 		writer.Write(buf[:n])
 	}
+}
+
+func noticeJobnet(writer http.ResponseWriter, request *http.Request) {
+	jobnetwork := request.FormValue("jobnetwork")
+	//	bucket := request.FormValue("bucket")
+	//	file := request.FormValue("file")
+
+	//dynamicJobnetName, err := define.ReplaceJobnetTemplate(jobnetwork, bucket, file)
+	//	if err != nil {
+	//		writer.WriteHeader(http.StatusNotFound)
+	//		return
+	//	}
+
+	//TODO realtime's stdout
+	output := "{\"status\":0,\"message\":\"\",\"pid\":1234,\"network\":{\"instance\":123,\"name\":\"" + jobnetwork + "\"}}"
+
+	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+	writer.Write([]byte(output))
 }
--- handler_test.go
+++ handler_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -30,8 +33,56 @@ func TestShowJSONCache(t *testing.T) {
 	}
 }
 
+func TestNoticeJobnet(t *testing.T) {
+	server := httptest.NewServer(setupHandler())
+	defer server.Close()
+
+	cacheDir := filepath.Join(os.TempDir(), "gocuto")
+	os.MkdirAll(cacheDir, 0777)
+	cacheFile := filepath.Join(cacheDir, "testjobnet.json")
+	f, err := os.Create(cacheFile)
+	if err != nil {
+		t.Fatalf("File create error: %s", err)
+	}
+	f.WriteString("abcd")
+	f.Close()
+	defer os.Remove(cacheFile)
+
+	form := make(url.Values)
+	form.Set("jobnetwork", "testjobnet")
+	form.Set("bucket", "bucket1")
+	form.Set("file", "testdata")
+
+	output := testPostFormMessages(t, server.URL+"/notice", form)
+	fmt.Println(output)
+	if !strings.Contains(output, "testjobnet") {
+		t.Errorf("output 0size")
+	}
+}
+
 func testGetMessages(t *testing.T, requestURL string) string {
 	res, err := http.Get(requestURL)
+	if err != nil {
+		t.Fatalf("Error occured: %s", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		t.Logf("URL[%s]", requestURL)
+		t.Errorf("res.StatusCode => %d, want %d", res.StatusCode, http.StatusOK)
+	}
+
+	resMsg, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Responce read error occured: %s", err)
+	}
+
+	return string(resMsg)
+}
+
+func testPostFormMessages(t *testing.T, requestURL string, form url.Values) string {
+	res, err := http.PostForm(requestURL, form)
+	//	json := `{"jobnetwork":"testjobnet","bucket":"bucket1","file":"testdata"}`
+	//	res, err := http.Post(requestURL, "application/json", strings.NewReader(json))
 	if err != nil {
 		t.Fatalf("Error occured: %s", err)
 	}
